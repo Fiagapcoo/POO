4dd9cd34d3244c4280588d46316c1ef2586dfefa
wrs <22085613+wi-r-es@users.noreply.github.com>
2023-11-07 02:40:31 +0000

    Add files via upload

---
+++ Library/Blackjack.cpp
@@ -0,0 +1,17 @@
+//
+// Created on 06/11/23.
+//
+
+#include "Headers/Blackjack.h"
+
+Blackjack::Blackjack(int posX, int posY) {
+
+}
+
+Blackjack::~Blackjack() {
+
+}
+
+void Machine::Play(User* user){
+
+}
